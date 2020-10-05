b8e335bc8b0b7e58e45283453931588adbe18b62
SaiSree-0517 <69571289+SaiSree-0517@users.noreply.github.com>
2020-10-05 14:12:00 +0530

    Add files via upload

---
+++ AreaOfRectangle.java
@@ -0,0 +1,31 @@
+import java.lang.*;
+import java.util.*;
+class Rectangle
+{
+	float l,b;
+	Rectangle(float l,float b)
+	{
+		this.l=l;
+		this.b=b;
+		
+	}
+	void display()
+	{
+		System.out.print("Area="+(l*b));
+	}
+
+}
+ class AreaOfRectangle
+{
+    
+	public static void main(String args[])
+
+    	{
+
+       		Rectangle r=new Rectangle(3.5f,7.5f);
+		r.display();
+		
+	
+}
+
+}
